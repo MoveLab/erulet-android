8faf76ef6854fa4afe41828bbdfbf8d2373a807a
a.escobar <a.escobar@creaf.uab.es>
2014-08-05 13:51:02 +0200

    Added preferences activity to expand

---
+++ SUDEAU/src/net/movelab/sudeau/EruletPreferencesActivity.java
@@ -0,0 +1,87 @@
+package net.movelab.sudeau;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+public class EruletPreferencesActivity extends Activity {
+	
+	private RadioGroup rbPreferredLocaleSelector;
+	private SharedPreferences mPreferences;
+	private SharedPreferences.Editor mPrefEditor;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.preferences);
+		mPreferences = getSharedPreferences("EruletPreferences", MODE_PRIVATE);
+	    mPrefEditor = mPreferences.edit();
+	    rbPreferredLocaleSelector = (RadioGroup)findViewById(R.id.radioLang);
+		updateRadioButtonState();
+	}
+	
+	private void updateRadioButtonState(){
+		String pref_locale = mPreferences.getString("pref_locale", "");
+		if(pref_locale.equalsIgnoreCase("ca")){
+			rbPreferredLocaleSelector.check(R.id.rbCatala);
+		}else if(pref_locale.equalsIgnoreCase("ar")){
+			rbPreferredLocaleSelector.check(R.id.rbAranes);
+		}else if(pref_locale.equalsIgnoreCase("es")){
+			rbPreferredLocaleSelector.check(R.id.rbSpanish);
+		}else if(pref_locale.equalsIgnoreCase("fr")){
+			rbPreferredLocaleSelector.check(R.id.rbFrench);
+		}else if(pref_locale.equalsIgnoreCase("en")){
+			rbPreferredLocaleSelector.check(R.id.rbEnglish);
+		}else{
+			rbPreferredLocaleSelector.check(R.id.rbCatala);
+		}
+	}
+	
+	public void onRadioButtonClicked(View view){
+	    boolean checked = ((RadioButton) view).isChecked();	    	    
+	    switch(view.getId()) {
+	        case R.id.rbCatala:
+	            if (checked){	            	
+	            	mPrefEditor.putString("pref_locale", "ca");
+	            	mPrefEditor.commit();	            	
+	            }
+	            break;
+	        case R.id.rbAranes:
+	            if (checked){
+	            	mPrefEditor.putString("pref_locale", "ar");
+	            	mPrefEditor.commit();	            	
+	            }
+	            break;
+	        case R.id.rbSpanish:
+	            if (checked){
+	            	mPrefEditor.putString("pref_locale", "es");
+	            	mPrefEditor.commit();	            	
+	            }
+	            break;
+	        case R.id.rbFrench:
+	            if (checked){
+	            	mPrefEditor.putString("pref_locale", "fr");
+	            	mPrefEditor.commit();	            	
+	            }
+	            break;
+	        case R.id.rbEnglish:
+	            if (checked){
+	            	mPrefEditor.putString("pref_locale", "en");
+	            	mPrefEditor.commit();	            	
+	            }
+	            break;
+	        default:
+	        	if(checked){
+	        		mPrefEditor.putString("pref_locale", "");
+	            	mPrefEditor.commit();	            	
+	        	}
+	        	break;
+	    }
+	    Toast.makeText(getApplicationContext(), "Cal reiniciar", Toast.LENGTH_LONG).show();
+	}
+}
--- SUDEAU/src/net/movelab/sudeau/Switchboard.java
+++ SUDEAU/src/net/movelab/sudeau/Switchboard.java
@@ -2,10 +2,13 @@ package net.movelab.sudeau;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Switchboard extends Activity {
 	
@@ -13,19 +16,48 @@ public class Switchboard extends Activity {
 	private Button btn_security;
 	private Button btn_routes;
 	private Button btn_credits;
+	private EruletApp app;
+	
+	private int group1 = 1;
+	private int first_id = Menu.FIRST;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (app == null) { app = (EruletApp) getApplicationContext(); }
 		setContentView(R.layout.activity_switchboard);
 		initButtons();
+		//See if we need to display the user registration
+        SharedPreferences preferences = getSharedPreferences("EruletPreferences", MODE_PRIVATE);
+        boolean firstTime = preferences.getBoolean("first_time", true);
+        
+        if (firstTime) {
+            //Intent intent = new Intent(this, RegistrationActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//            intent.putExtra("first_time", true);
+//            startActivity(intent);
+        	Toast.makeText(this, "First time!", Toast.LENGTH_SHORT).show();
+            preferences.edit().putBoolean("first_time", false).apply();
+        }
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.switchboard, menu);
+		menu.add(group1, first_id, first_id, "Configuració");
+		//getMenuInflater().inflate(R.menu.switchboard, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case 1:
+				Intent i = new Intent(Switchboard.this,EruletPreferencesActivity.class);				
+				startActivity(i);
+				break;
+			default:				
+				break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	private void initButtons() {
